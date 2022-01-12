3e86262803933204a15dd5188287ca6f739dd31f
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-01-12 13:22:45 +0100

    draw path

--- javascripts/intro.js
+++ javascripts/intro.js
@@ -37,8 +37,41 @@ stroke(); // Draws the shape by stroking its outline.
 fill(); // Draws a solid shape by filling the path's content area.
 */
 
+/*
+Moving the pen
+One very useful function, which doesn’t actually draw anything but becomes part of the path list described above, is the moveTo() function. You can probably best think of this as lifting a pen
+or pencil from one spot on a piece of paper and placing it on the next.
+Method:
+moveTo(x, y); // Moves the pen to the coordinates specified by x and y.
+*/
+
+/*
+LINE DRAWING
+For drawing straight lines, use the lineTo() method.
+lineTo(x, y); // Draws a line from the current drawing position to the position specified by x and y.
+*/
+
+
 // ****************************
-// draw path:
+// draw path
 // ****************************
 
-ctx.beginPath()
+// start the path
+ctx.beginPath();
+// starting position is x=50, y=50
+ctx.moveTo(50, 50);
+// draw the line that has final coordinates x=250, y=50
+ctx.lineTo(250, 50);
+
+// .stroke() executes the drawing
+ctx.stroke();
+
+// start a new line from these coordinates: x=250, y=50
+ctx.moveTo(250, 50);
+// draw the line that has final coordinates x=250, y=100
+ctx.lineTo(250, 100);
+// .stroke() executes the drawing
+ctx.stroke();
+
+// close the path
+ctx.closePath();
